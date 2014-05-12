92b3452dcd5f212f03152d820006ff9f9039c926
Thomas Vestergaard Trolle <thomas@tveon.dk>
2014-05-12 23:27:28 +0200

    Testing with Chrome

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -6,13 +6,13 @@ var geotoolsApp = angular.module('geotoolsApp', []);
 
 geotoolsApp.filter('charNumber', function () {
 
-    const DK_ALPHABET = {
+    var DK_ALPHABET = {
         'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
         'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
         't': 20, 'u': 21, 'v': 22, 'x': 23, 'y': 24, 'z': 25, 'æ': 26, 'ø': 27, 'å': 28
     };
 
-    const DK_ALPHABETW = {
+    var DK_ALPHABETW = {
         'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
         'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19,
         't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 'æ': 27, 'ø': 28, 'å': 29
@@ -43,7 +43,7 @@ geotoolsApp.filter('sumOfDigits', function () {
             var result = 0;
             for (var i = 0; i < s.length; i++) {
                 var number = s.charAt(i);
-                if (!isNaN(number)) {
+                if (!isNaN(number)) { //TODO - handle " "
                     result = result + parseInt(number);
                 }
             }
